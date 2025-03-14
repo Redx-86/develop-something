070f57ff6c007c9f1aca726d0636c4e781e3db5e
Thrint <orkun.vue@gmail.com>
2025-03-14 10:58:08 +0300

    sa
    as

--- backend/models/form_models/il_ilce.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const ilIlceSchema = new mongoose.Schema({
-    il: { type: String, required: true },
-    ilceler: [{ type: String }]
-});
-module.exports = mongoose.model('IlIlce', ilIlceSchema, 'il_ilce');
--- backend/routes/api/form_routes/il_ilce_Routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const illerController = require('../../../controllers/illerController');
-
-router.get('/iller', illerController.getAllCities);
-
-router.get('/iller/:il', illerController.getDistrictsByCity);
-
-module.exports = router;
